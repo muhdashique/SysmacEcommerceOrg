166705e84f702f1b53631a20e067622114ea39c4
DESKTOP-HEIP9LK\user <muhammedashique8281@gmail.com>
2025-07-25 11:16:02 +0530

    api edit page updated

--- sysmac_ecommerce_project/settings.py
+++ sysmac_ecommerce_project/settings.py
@@ -106,6 +106,7 @@ DATABASES = {
         'PORT': '5432',  # Default PostgreSQL port is 5432
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
