2255e1591a614791d1acb0c32360676589db2e6a
DESKTOP-HEIP9LK\user <muhammedashique8281@gmail.com>
2025-08-08 10:36:46 +0530

    pro_view page

--- sysmac_ecommerce_project/settings.py
+++ sysmac_ecommerce_project/settings.py
@@ -84,28 +84,28 @@ WSGI_APPLICATION = 'sysmac_ecommerce_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sysmacdb',  # Replace with your database name
-#         'USER': 'postgres',  # Replace with your database username
-#         'PASSWORD': '1234',  # Replace with your database password
-#         'HOST': 'localhost',  # For local development, use 'localhost' or '127.0.0.1'
-#         'PORT': '5432',  # Default PostgreSQL port is 5432
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sysmac_ecommerce',  # Replace with your database name
+        'NAME': 'sysmacdb',  # Replace with your database name
         'USER': 'postgres',  # Replace with your database username
-        'PASSWORD': 'info@imc',  # Replace with your database password
+        'PASSWORD': '1234',  # Replace with your database password
         'HOST': 'localhost',  # For local development, use 'localhost' or '127.0.0.1'
         'PORT': '5432',  # Default PostgreSQL port is 5432
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sysmac_ecommerce',  # Replace with your database name
+#         'USER': 'postgres',  # Replace with your database username
+#         'PASSWORD': 'info@imc',  # Replace with your database password
+#         'HOST': 'localhost',  # For local development, use 'localhost' or '127.0.0.1'
+#         'PORT': '5432',  # Default PostgreSQL port is 5432
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
